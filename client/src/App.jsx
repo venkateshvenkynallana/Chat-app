b9bd68f4016b019d43d62bd1e5c1c1ed7aacd484
venky <venkateshnallana2003@gmail.com>
2025-11-29 19:21:47 +0530

    account created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from './pages/HomePage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
 import { Toaster } from "react-hot-toast";
-import { AuthContext } from '../context/AuthContext.jsx'
+import { AuthContext } from './context/AuthContext.jsx'
 
 const App = () => {
 
@@ -15,7 +15,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
         />
-        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        <Route path='/login' element={authUser ? <LoginPage /> : <Navigate to={"/"} />}
         />
         <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />}
         />
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import assets from '../assets/assets'
+import { AuthContext } from '../context/AuthContext';
+import toast from 'react-hot-toast';
 
 const LoginPage = () => {
 
@@ -10,6 +12,8 @@ const LoginPage = () => {
   const [bio, setBio] = useState("");
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
+  const { login } = useContext(AuthContext);
+
   const onSubmitHandler = (event) =>{
     event.preventDefault();
 
@@ -17,6 +21,8 @@ const LoginPage = () => {
       setIsDataSubmitted(true);
       return;
     }
+    login(currState === "Sign up" ? 'signup' : 'login', {fullName, email, password, bio})
+    
   }
 
   return (
